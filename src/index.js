745ed206f785c93bb5cec8a7f37c85be96d28a80
Kemberly Eliscar <kemberlyeliscar@Kemberlys-Air-3.home>
2019-09-04 18:21:57 -0400

    built localstorage function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
+import { useLocalStorage } from './components/Hooks/useLocalStorage'
 
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
