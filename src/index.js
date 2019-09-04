d95538798b39793eb5b99f6589fb816e03255e56
Kemberly Eliscar <kemberlyeliscar@Kemberlys-Air-3.home>
2019-09-04 18:43:27 -0400

    created localstorage custom hook

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
-import { useLocalStorage } from './components/Hooks/useLocalStorage'
+
 
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
 
 import "./styles.scss";
 
+import useLocalStorage from './components/Hooks/useLocalStorage';
+
 const App = () => {
   const [coinData, setCoinData] = useState([]);
+  const [local, setLocal] = useLocalStorage('')
 
   useEffect(() => {
     axios
